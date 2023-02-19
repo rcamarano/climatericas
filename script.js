b51d753aaa705347bb029343478daaa031a696a3
Rogerio Camarano <rogeriocamarano@gmail.com>
2023-02-19 13:50:37 -0300

    Subindo projeto que já estava feito localmente

---
+++ script.js
@@ -0,0 +1,47 @@
+// document.getElementById("myForm").addEventListener("submit", function(event){
+//   event.preventDefault();
+//   document.getElementById("download").disabled = false;
+// });
+  
+// let name = document.getElementById("name");
+// let email = document.getElementById("email");
+// let phone = document.getElementById("phone");
+// let downloadBtn = document.getElementById("download");
+
+// downloadBtn.disabled = true;
+
+// form.addEventListener("submit", function(event) {
+//   event.preventDefault();
+
+//   if (name.value && email.value && phone.value) {
+//     if (validateEmail(email.value) && validatePhone(phone.value)) {
+//       downloadBtn.disabled = false;
+//     } else {
+//       alert("Favor inserir um número de celular válido.");
+//     }
+//   } else {
+//     alert("Preencha todos os campos.");
+//   }
+// });
+
+// function validateEmail(email) {
+//   const email = document.getElementById("email").value;
+//   const result = "";
+  
+//   if (!result.test(email)) {
+//     alert("Email inválido!");
+//     return false;
+//   }
+//   return true;
+// }
+
+// function validatePhone(phone) {
+//   const phone = document.getElementById("phone").value;
+//   const result = "";
+  
+//   if (!result.test(phone)) {
+//     alert("Número de telefone inválido!");
+//     return false;
+//   }
+//   return true;
+// }
